17124843ff02ff204725d51654466889b8d0222e
emre <kuruem@itu.edu.tr>
2017-11-18 20:21:06 +0300

    import duzenleme

--- accountApp/src/test/java/com/aric/samples/account/model/AccountTest.java
+++ accountApp/src/test/java/com/aric/samples/account/model/AccountTest.java
@@ -1,11 +1,10 @@
 package com.aric.samples.account.model;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import junit.framework.Assert;
 
 
 /**
