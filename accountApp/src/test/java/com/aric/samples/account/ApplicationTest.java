30d473c2496d219b61916e85f96ec63612d27721
emre <kuruem@itu.edu.tr>
2017-11-18 20:14:03 +0300

    deneme

--- accountApp/src/test/java/com/aric/samples/account/ApplicationTest.java
+++ accountApp/src/test/java/com/aric/samples/account/ApplicationTest.java
@@ -1,5 +1,15 @@
 package com.aric.samples.account;
 
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 public class ApplicationTest {
+
+	@Rule
+    public ExpectedException exception = ExpectedException.none();
+	
 
 }
