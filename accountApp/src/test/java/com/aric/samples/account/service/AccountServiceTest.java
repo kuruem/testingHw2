5e876947aa49e1824a111a73d32bd3ccd42bff8d
emre <kuruem@itu.edu.tr>
2017-11-19 16:38:47 +0300

    account service tests

--- accountApp/src/test/java/com/aric/samples/account/service/AccountServiceTest.java
+++ accountApp/src/test/java/com/aric/samples/account/service/AccountServiceTest.java
@@ -1,5 +1,87 @@
 package com.aric.samples.account.service;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import java.util.Optional;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import com.aric.samples.account.model.Account;
+import com.aric.samples.account.repository.AccountRepository;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AccountServiceTest {
+	private static List<Account> account;
+	private static List<Account> accountson;
+	private static Account account1;
+	private static Account account2; 
+	private static Account account3; 
+	private static final String FIRST_NAME1= "kursat";
+	private static final String LAST_NAME1= "yasar";
+	private static final String FIRST_NAME2= "emre";
+	private static final String LAST_NAME2= "kuru";
+	private static final long tckn1=161;
+	private static final long tckn2=292;
+	private static final long id1=1;
+	private static final long id2=2;
+	private static final long balance1=1500;
+	private static final long balance2=2500;
+	private AccountRepository mockAccountRepository;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		account =  new ArrayList<Account>();
+		accountson =  new ArrayList<Account>();
+		account1= new Account();
+		account2=new Account();
+		account1.setId(id1);
+		account2.setId(id2);
+		account1.setOwnerFirstName(FIRST_NAME1);
+		account2.setOwnerFirstName(FIRST_NAME2);
+		account1.setOwnerLastName(LAST_NAME1);
+		account2.setOwnerLastName(LAST_NAME2);
+		account1.setOwnerTckn(tckn1);
+		account2.setOwnerTckn(tckn2);
+		account1.setBalance(balance1);
+		account2.setBalance(balance2);
+		account.add(account1);
+		account.add(account2);
+	}
+	
+	@Autowired
+	private AccountService accountingService;
+	
+	
+	@Before
+	public void setUp() {
+		mockAccountRepository = Mockito.mock(AccountRepository.class);
+		ReflectionTestUtils.setField(accountingService,"accountRepository",mockAccountRepository);
+		Mockito.when(mockAccountRepository.findByOwnerTckn(161)).thenReturn(Arrays.asList(account1));
+		Mockito.when(mockAccountRepository.findByOwnerTckn(292)).thenReturn(Arrays.asList(account2));
+	}
+	
+	@Test
+	public void findPersonsByTcknTest() throws Exception{
+		accountson = accountingService.findPersonsByTckn(161);
+		Mockito.verify(mockAccountRepository);
+		Assert.assertEquals("yanlis ismi buldu","kursat", accountson.get(0).getOwnerFirstName());
+		Assert.assertEquals("yasar", accountson.get(0).getOwnerLastName());
+		Assert.assertEquals("balance dogru degil",balance1, accountson.get(0).getBalance(),0);
+		Assert.assertEquals("ıdler birbirine esit degil",id1, accountson.get(0).getId());
+		
+	}
+	
 	
 }
