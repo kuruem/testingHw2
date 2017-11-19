cd3d9b101b5a125e99a6bcf102224c0ec12804de
emre <kuruem@itu.edu.tr>
2017-11-19 20:42:28 +0300

    account controller tests

--- accountApp/src/test/java/com/aric/samples/account/controller/AccountControllerTest.java
+++ accountApp/src/test/java/com/aric/samples/account/controller/AccountControllerTest.java
@@ -1,27 +1,18 @@
 package com.aric.samples.account.controller;
 
 
-import org.junit.Before;
-import org.junit.BeforeClass;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.aric.samples.account.model.Account;
-import com.aric.samples.account.service.AccountService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -32,8 +23,22 @@ public class AccountControllerTest {
     private MockMvc mockMvc;
     
     @Test
-    public void testAccountController() throws Exception {    	
-        this.mockMvc.perform(get("/query")).andExpect(status().isOk());
+    public void testAccountRequest() throws Exception {    	
+        this.mockMvc.perform(get("/query")).andExpect(status().isOk()).andExpect(content().string(containsString("query method is working")));
 	
-    }  
+    }
+    
+    public void testDepositRequest() throws Exception {    	
+        this.mockMvc.perform(get("/deposit")).andExpect(status().isOk()).andExpect(content().string(containsString("deposit method working")));
+	
+    }
+    public void testEftRequest() throws Exception {    	
+        this.mockMvc.perform(get("/eft")).andExpect(status().isOk()).andExpect(content().string(containsString("eft method working")));
+	
+    }
+    
+    //çağıırlan metodlar için ... mewthod working mesajı üretir. test bu şekilde kontrole ediliyor
+    
+    
+    
 }
